dc36d444aab71d2fab9ea3f9d89e97fe4edccbb5
VAIBHAV SRIVASTAVA <86517958+myhacktech@users.noreply.github.com>
2023-12-19 03:42:50 +0530

    Add files via upload

---
+++ Frontend/src/Pages/Index/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+const Index = () => {
+  return (
+    <div>
+      <NavLink to="/login">Login</NavLink>
+    </div>
+  )
+}
+
+export default Index
